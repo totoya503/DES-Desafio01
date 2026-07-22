6d8b1d63c991a8a835249cd7d7f1ffbedfa8a8cd
Rickpoio <esricardo.ivan03@gmail.com>
2026-07-21 21:51:52 -0600

    Implementacion de la plantilla a utilizar para la realizacion del proyecto

---
+++ DES - Desafio 1/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+using DES___Desafio_1.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace DES___Desafio_1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
