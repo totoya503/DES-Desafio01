e3e47593b2aa9b5e9d42b73132ebdbc36b756e43
Rickpoio <esricardo.ivan03@gmail.com>
2026-07-23 00:04:48 -0600

    Push insano de cambios para trabajar con la creacion de la base de datos y la tabla departamentos

--- DES - Desafio 1/obj/Debug/net10.0/DES - Desafio 1.AssemblyInfo.cs
+++ DES - Desafio 1/obj/Debug/net10.0/DES - Desafio 1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DES - Desafio 1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4063efbb84b97628277e732082f53216bf424033")]
 [assembly: System.Reflection.AssemblyProductAttribute("DES - Desafio 1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DES - Desafio 1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
