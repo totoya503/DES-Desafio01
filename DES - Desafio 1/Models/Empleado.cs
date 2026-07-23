56a793b2aa1a111b74e4b28b65210ff393d74628
totoya503 <angeleme503@gmail.com>
2026-07-23 01:41:03 -0600

    Integracion modelo Empleado y relacion con Departamento

--- DES - Desafio 1/Models/Empleado.cs
+++ DES - Desafio 1/Models/Empleado.cs
@@ -1,16 +1,16 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace DES___Desafio_1.Models
 {
     public class Empleado
     {
+        [Key]
         public int EmpleadoId { get; set; }
 
         [Required]
         [StringLength(100, MinimumLength = 2)]
         public string Nombre { get; set; } = string.Empty;
-
-        public int DepartamentoId { get; set; }
 
         [Required]
         public DateTime FechaNacimiento { get; set; }
@@ -24,5 +24,10 @@ namespace DES___Desafio_1.Models
 
         public string? Descripcion { get; set; }
 
+        [Required]
+        public int DepartamentoId { get; set; }
+
+        [ForeignKey("DepartamentoId")]
+        public virtual Departamento? Departamento { get; set; }
     }
 }
--- DES - Desafio 1/Program.cs
+++ DES - Desafio 1/Program.cs
@@ -2,14 +2,34 @@ using Microsoft.EntityFrameworkCore;
 using DES___Desafio_1.Models;
 
 var builder = WebApplication.CreateBuilder(args);
-// Define la carpeta base de ejecución para que |DataDirectory| apunte al directorio local del proyecto
+
+// Define la carpeta base de ejecución
 string basePath = AppDomain.CurrentDomain.BaseDirectory;
 AppDomain.CurrentDomain.SetData("DataDirectory", basePath);
 
 builder.Services.AddControllersWithViews();
 
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseNpgsql(
-        builder.Configuration.GetConnectionString("PostgresConnection")));
+builder.Services.AddDbContext<EmpleadosDbContext>(options =>
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
